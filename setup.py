a5747c95cae70dacab8833e41e42cd02de8b7c40
Anıl Kaynar <aanilkay@gmail.com>
2020-02-05 16:32:30 +0300

    Readme Updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="turkishfullnamegenerator", 
-    version="0.0.4",
+    version="0.0.5",
     author="anilkay",
     author_email="aanilkay@gmail.com",
     description="Generate full turkish names ",
