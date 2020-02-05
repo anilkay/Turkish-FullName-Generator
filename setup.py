161bd3e0c71c263018182065e3517a38084b69b3
Anıl Kaynar <aanilkay@gmail.com>
2020-02-05 16:24:50 +0300

    pypi long description added.

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="turkishfullnamegenerator", 
-    version="0.0.3",
+    version="0.0.4",
     author="anilkay",
     author_email="aanilkay@gmail.com",
     description="Generate full turkish names ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/anilkay/Turkish-FullName-Generator",
     packages=["turkishfullname"],
     keywords = ['name generator', 'turk', 'turkish'], 
