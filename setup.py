0759c956cec2324c449e9ac2eab7bdb116f08d64
Anıl Kaynar <aanilkay@gmail.com>
2020-02-05 14:14:56 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="turkish-fullname-generator", # Replace with your own username
+    version="0.0.1",
+    author="anilkay",
+    author_email="",
+    description="Generate full turkish names ",
+    long_description=long_description,
+    url="https://github.com/anilkay/Turkish-FullName-Generator",
+    packages="turkishfullname",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
